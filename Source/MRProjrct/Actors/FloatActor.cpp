de9460f8e79783edc946091cc676bd48170fb3f9
ehnehzuw <ehnehzuw@gmail.com>
2026-05-27 16:13:27 +0100

    iteration

--- Source/MRProjrct/Actors/FloatActor.cpp
+++ Source/MRProjrct/Actors/FloatActor.cpp
@@ -70,7 +70,7 @@ void AFloatActor::Tick(float DeltaTime)
 
 	RunningTime += DeltaTime;
 
-	const float DesiredZ = HoverOriginZ + TargetHeight + FMath::Sin(RunningTime * FloatSpeed) * FloatAmplitude;
+	const float DesiredZ = HoverOriginZ + TargetHeight + FMath::Sin(RunningTime * FloatSpeed) * FloatAmplitude* 0.25f;
 
 	FVector NewLocation = GetActorLocation();
 	NewLocation.Z = DesiredZ;
